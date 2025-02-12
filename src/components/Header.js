bd9f8c84b156f8395a7f7b832b5e5bd28da02d9e
Sean Fagan <seanfagan@Seans-MacBook-Pro.local>
2025-02-12 08:27:32 -0900

    Removed logo from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,6 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <a href="https://akcasc.org/" target="_blank" rel="noopener noreferrer">
-        <img src="/AK CASC horiz white.png" alt="Alaska Climate Adaptation Science Center Logo" className="header-logo" />
-      </a>
       <div>
         <h1>Mendenhall Glacier Flood Tracker</h1>
       </div>
@@ -15,4 +12,3 @@ const Header = () => {
 };
 
 export default Header;
-
